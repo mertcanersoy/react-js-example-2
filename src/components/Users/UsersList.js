d7734b81cd6f6533df809080960da35dbbccbeda
Mertcan Ersoy <mertcannersoy@gmail.com>
2023-03-19 03:59:48 +0300

    fix

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import style from "./UsersList.module.css";
 
 const UsersList = (props) => {
+  if (props.users.length === 0) {
+    return null; //if there is no user hide card
+  }
   return (
     <Card className={style.users}>
       <ul>
